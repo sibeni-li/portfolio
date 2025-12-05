691260e4c2fdbd85529987600b6b5cd4a309fb34
Lisa Sibeni <146650473+sibeni-li@users.noreply.github.com>
2025-12-05 02:46:02 +0000

    Minimal app setup

--- app.py
+++ app.py
@@ -2,6 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
