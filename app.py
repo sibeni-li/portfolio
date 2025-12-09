e6c681ec07c43ef10c519698fcf1e2b0afb21ffa
Lisa Sibeni <146650473+sibeni-li@users.noreply.github.com>
2025-12-09 15:15:02 +0000

    session config for development, imports and admin routes added

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+from dotenv import load_dotenv
+from werkzeug.security import check_password_hash
+
+from helpers import login_required
+from schema import create_table, insert_project, insert_language
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+load_dotenv()
+create_table()
+
 
 @app.after_request
 def after_request(response):
@@ -21,6 +37,59 @@ def about():
     return render_template('about.html')
 
 
+# TODO
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
+
+# TODO
+@app.route('/project/<int:project_id>')
+def project(project_id):
+    # Handle project details here
+    pass
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        if username == os.getenv("ADMIN_USERNAME") and check_password_hash(os.getenv("ADMIN_PASSWORD"), password):
+            session["logged_in"] = True
+            return redirect("/admin")
+        else:            
+            return render_template("login.html", error="Invalid username or password")
+    return render_template('login.html')
+
+
+@login_required
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect('/')
+
+
+@login_required
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if not session.get('logged_in'):
+        return redirect('/login')
+    
+    if request.method == 'POST':
+        # Handle admin actions here
+        project_name = request.form.get("project-name")
+        desc = request.form.get("desc")
+        img = request.form.get("project-img")
+        url = request.form.get("project-url")
+        github = request.form.get("project-github")
+        techs = request.form.get("project-tech")
+
+        project_id = insert_project(project_name, desc, img, url, github)
+        
+        if project_id and techs:
+            for tech in techs.split(','):
+                insert_language(project_id, tech.strip())
+
+    return render_template('admin.html')
